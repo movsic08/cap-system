1208d82b3be1073b78be004b2d14581b36aeec44
Jericho <jerichobantiquete@gmail.com>
2023-09-27 17:54:33 +0800

    feat: baptismal schedule form and finalization form

---
+++ resources/views/landingpage/schedule-events/burial/finalize-burial-form.blade.php
@@ -0,0 +1,346 @@
+<x-modal name="confirm-form">
+
+
+    <h2 class="text-lg font-medium text-gray-900 mt-6 mx-6">
+        {{ __('Please review your data before final submission.') }}
+    </h2>
+
+
+    <div class="mt-4 grid grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-6 px-6">
+        <div class="sm:col-span-3">
+            <span class="block text-sm font-medium leading-6 text-gray-600">
+                First Name
+            </span>
+            <div class="mt-2">
+                <div id="display1"
+                    class="block w-full rounded-md border-0 px-2 py-1.5 ring-2 ring-indigo-700 text-gray-900 shadow-sm  placeholder:text-gray-400   sm:text-sm sm:leading-6">
+                </div>
+            </div>
+        </div>
+
+        <div class="sm:col-span-3">
+            <span class="block text-sm font-medium leading-6 text-gray-600">
+                Email Address
+            </span>
+            <div class="mt-2">
+                <div id="display2"
+                    class="block w-full rounded-md border-0 px-2 py-1.5 ring-2 ring-indigo-700 text-gray-900 shadow-sm  placeholder:text-gray-400   sm:text-sm sm:leading-6">
+                </div>
+            </div>
+        </div>
+
+        <div class="sm:col-span-3">
+            <span class="block text-sm font-medium leading-6 text-gray-600">
+                Deceased Name +
+            </span>
+            <div class="mt-2">
+                <div id="display3"
+                    class="block w-full rounded-md border-0 px-2 py-1.5 ring-2 ring-indigo-700 text-gray-900 shadow-sm  placeholder:text-gray-400   sm:text-sm sm:leading-6">
+                </div>
+            </div>
+        </div>
+
+        <div class="sm:col-span-3">
+            <span class="block text-sm font-medium leading-6 text-gray-600">
+                Deceased Age +
+            </span>
+            <div class="mt-2">
+                <div id="display4"
+                    class="block w-full rounded-md border-0 px-2 py-1.5 ring-2 ring-indigo-700 text-gray-900 shadow-sm  placeholder:text-gray-400   sm:text-sm sm:leading-6">
+                </div>
+            </div>
+        </div>
+
+        <div class="sm:col-span-3">
+            <span class="block text-sm font-medium leading-6 text-gray-600">
+                Deceased Status +
+            </span>
+            <div class="mt-2">
+                <div id="display5"
+                    class="block w-full rounded-md border-0 px-2 py-1.5 ring-2 ring-indigo-700 text-gray-900 shadow-sm  placeholder:text-gray-400   sm:text-sm sm:leading-6">
+                </div>
+            </div>
+        </div>
+
+        <div class="sm:col-span-3">
+            <span class="block text-sm font-medium leading-6 text-gray-600">
+                Parents/Husband/Wife
+            </span>
+            <div class="mt-2">
+                <div id="display6"
+                    class="block w-full rounded-md border-0 px-2 py-1.5 ring-2 ring-indigo-700 text-gray-900 shadow-sm  placeholder:text-gray-400   sm:text-sm sm:leading-6">
+                </div>
+            </div>
+        </div>
+
+        <div class="sm:col-span-3">
+            <span class="block text-sm font-medium leading-6 text-gray-600">
+                Cause of Death +
+            </span>
+            <div class="mt-2">
+                <div id="display7"
+                    class="block w-full rounded-md border-0 px-2 py-1.5 ring-2 ring-indigo-700 text-gray-900 shadow-sm  placeholder:text-gray-400   sm:text-sm sm:leading-6">
+                </div>
+            </div>
+        </div>
+
+        <div class="sm:col-span-3">
+            <span class="block text-sm font-medium leading-6 text-gray-600">
+                Date of Death
+            </span>
+            <div class="mt-2">
+                <div id="display8"
+                    class="block w-full rounded-md border-0 px-2 py-1.5 ring-2 ring-indigo-700 text-gray-900 shadow-sm  placeholder:text-gray-400   sm:text-sm sm:leading-6">
+                </div>
+            </div>
+        </div>
+
+        <div class="sm:col-span-3">
+            <span class="block text-sm font-medium leading-6 text-gray-600">
+                Cemetery
+            </span>
+            <div class="mt-2">
+                <div id="display9"
+                    class="block w-full rounded-md border-0 px-2 py-1.5 ring-2 ring-indigo-700 text-gray-900 shadow-sm  placeholder:text-gray-400   sm:text-sm sm:leading-6">
+                </div>
+            </div>
+        </div>
+
+        <div class="sm:col-span-3">
+            <span class="block text-sm font-medium leading-6 text-gray-600">
+                Minister
+            </span>
+            <div class="mt-2">
+                <div id="display10"
+                    class="block w-full rounded-md border-0 px-2 py-1.5 ring-2 ring-indigo-700 text-gray-900 shadow-sm  placeholder:text-gray-400   sm:text-sm sm:leading-6">
+                </div>
+            </div>
+        </div>
+
+        <div class="sm:col-span-3">
+            <span class="block text-sm font-medium leading-6 text-gray-600">
+                NON/UT
+            </span>
+            <div class="mt-2">
+                <div id="display11"
+                    class="block w-full rounded-md border-0 px-2 py-1.5 ring-2 ring-indigo-700 text-gray-900 shadow-sm  placeholder:text-gray-400   sm:text-sm sm:leading-6">
+                </div>
+            </div>
+        </div>
+
+        <div class="sm:col-span-3">
+            <span class="block text-sm font-medium leading-6 text-gray-600">
+                Address
+            </span>
+            <div class="mt-2">
+                <div id="display12"
+                    class="block w-full rounded-md border-0 px-2 py-1.5 ring-2 ring-indigo-700 text-gray-900 shadow-sm  placeholder:text-gray-400   sm:text-sm sm:leading-6">
+                </div>
+            </div>
+        </div>
+
+        <div class="sm:col-span-3">
+            <span class="block text-sm font-medium leading-6 text-gray-600">
+                Desired Start Date & Time
+            </span>
+            <div class="mt-2">
+                <div id="display13"
+                    class="block w-full rounded-md border-0 px-2 py-1.5 ring-2 ring-indigo-700 text-gray-900 shadow-sm  placeholder:text-gray-400   sm:text-sm sm:leading-6">
+                </div>
+            </div>
+        </div>
+
+        <div class="sm:col-span-3">
+            <span class="block text-sm font-medium leading-6 text-gray-600">
+                Desired End Date & Time
+            </span>
+            <div class="mt-2">
+                <div id="display14"
+                    class="block w-full rounded-md border-0 px-2 py-1.5 ring-2 ring-indigo-700 text-gray-900 shadow-sm  placeholder:text-gray-400   sm:text-sm sm:leading-6">
+                </div>
+            </div>
+        </div>
+
+        <div class="sm:col-span-3">
+            <span class="block text-sm font-medium leading-6 text-gray-600">
+                Contact Number
+            </span>
+            <div class="mt-2">
+                <div id="display15"
+                    class="block w-full rounded-md border-0 px-2 py-1.5 ring-2 ring-indigo-700 text-gray-900 shadow-sm  placeholder:text-gray-400   sm:text-sm sm:leading-6">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="sm:col-span-6 mt-4 px-6">
+        <h2 class="font-bold text-slate-600">Checklist:</h2>
+    </div>
+
+    <div class="mt-4 grid grid-cols-1 px-6 gap-x-6 gap-y-4 sm:grid-cols-6">
+        <div class="col-span-6 ">
+            <div class="flex items-start">
+                <div class="flex items-center h-5">
+                    <input disabled id="display16" name="certificate_of_death" type="checkbox" value="1"
+                        class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded">
+                </div>
+                <div class="ml-3 text-sm">
+                    <span class="block text-sm font-medium leading-6 text-gray-600">Certificate of
+                        Death</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-span-6 ">
+            <div class="flex items-start">
+                <div class="flex items-center h-5">
+                    <input disabled id="display17" name="cemetery_lease_contract" type="checkbox" value="1"
+                        class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded">
+                </div>
+                <div class="ml-3 text-sm">
+                    <span class="block text-sm font-medium leading-6 text-gray-600">Cemetery Lease Contract</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-span-6 ">
+            <div class="flex items-start">
+                <div class="flex items-center h-5">
+                    <input disabled id="display18" name="burial_permit" type="checkbox" value="1"
+                        class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded">
+                </div>
+                <div class="ml-3 text-sm">
+                    <span class="block text-sm font-medium leading-6 text-gray-600">Burial Permit</span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="sm:col-span-6 mt-4 px-6">
+        <h2 class="font-bold text-slate-600">Offering:</h2>
+    </div>
+
+    <div class="mt-4 grid grid-cols-1 px-6 gap-x-6 gap-y-4 sm:grid-cols-6">
+        <div class="sm:col-span-2">
+            <span class="block text-sm font-medium leading-6 text-gray-600">
+                Ordinary
+            </span>
+            <div class="mt-2">
+                <div class="flex rounded-md shadow-sm py-0.5 ring-inset bg-white ring-indigo-700 ring-2 sm:max-w-md">
+                    <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">₱</span>
+                    <span id="display19"
+                        class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6">
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="sm:col-span-2">
+            <span class="block text-sm font-medium leading-6 text-gray-600">
+                With Mass
+            </span>
+            <div class="mt-2">
+                <div class="flex rounded-md shadow-sm py-0.5 ring-inset bg-white ring-indigo-700 ring-2 sm:max-w-md">
+                    <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">₱</span>
+                    <span id="display20"
+                        class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6">
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="sm:col-span-2">
+            <span class="block text-sm font-medium leading-6 text-gray-600">
+                Candles
+            </span>
+            <div class="mt-2">
+                <div class="flex rounded-md shadow-sm py-0.5 ring-inset bg-white ring-indigo-700 ring-2 sm:max-w-md">
+                    <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">₱</span>
+                    <span id="display21"
+                        class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6">
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="sm:col-span-2">
+            <span class="block text-sm font-medium leading-6 text-gray-600">
+                Lights
+            </span>
+            <div class="mt-2">
+                <div class="flex rounded-md shadow-sm py-0.5 ring-inset bg-white ring-indigo-700 ring-2 sm:max-w-md">
+                    <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">₱</span>
+                    <span id="display22"
+                        class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6">
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="sm:col-span-2">
+            <span class="block text-sm font-medium leading-6 text-gray-600">
+                Video Coverage
+            </span>
+            <div class="mt-2">
+                <div class="flex rounded-md shadow-sm py-0.5 ring-inset bg-white ring-indigo-700 ring-2 sm:max-w-md">
+                    <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">₱</span>
+                    <span id="display23"
+                        class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6">
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="sm:col-span-2">
+            <span class="block text-sm font-medium leading-6 text-gray-600">
+                Choir
+            </span>
+            <div class="mt-2">
+                <div class="flex rounded-md shadow-sm py-0.5 ring-inset bg-white ring-indigo-700 ring-2 sm:max-w-md">
+                    <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">₱</span>
+                    <span id="display24"
+                        class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6">
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="sm:col-span-2">
+            <span class="block text-sm font-medium leading-6 text-gray-600">
+                Cemetery Lot
+            </span>
+            <div class="mt-2">
+                <div class="flex rounded-md shadow-sm py-0.5 ring-inset bg-white ring-indigo-700 ring-2 sm:max-w-md">
+                    <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">₱</span>
+                    <span id="display25"
+                        class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6">
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="sm:col-span-2">
+            <span class="block text-sm font-medium leading-6 text-gray-600">
+                Etc.
+            </span>
+            <div class="mt-2">
+                <div class="flex rounded-md shadow-sm py-0.5 ring-inset bg-white ring-indigo-700 ring-2 sm:max-w-md">
+                    <span class="flex select-none items-center pl-3 text-gray-500 sm:text-sm">₱</span>
+                    <span id="display26"
+                        class="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6">
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="mt-4 sm:col-span-6 px-6 pb-6">
+        <span class="block text-sm font-medium leading-6 text-gray-600">
+            Message <span class="text-xs">(optional)</span>
+        </span>
+        <div class="mt-2">
+            <div class="block w-full rounded-md border-0 px-2 py-1.5 ring-2 ring-indigo-700 text-gray-900 shadow-sm  placeholder:text-gray-400  sm:text-sm sm:leading-6"
+                id="display27">
+            </div>
+        </div>
+    </div>
+
+    <div class="px-6 pb-6">
+        <button type="submit"
+            class="rounded-md bg-indigo-600 px-6 py-2 text-base font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            Submit
+        </button>
+    </div>
+</x-modal>
