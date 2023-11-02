6c2758c4323bbaeae2c9a5516c524a017bc461a4
Jericho <jerichobantiquete@gmail.com>
2023-11-02 11:30:08 +0800

    feat: guest layout mobile

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,12 +17,12 @@
 </head>
 
 <body
-    class="font-sans text-gray-900 bg-no-repeat bg-cover bg-[url('https://images.pexels.com/photos/9614109/pexels-photo-9614109.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')] antialiased">
+    class="font-sans h-screen  text-gray-900 bg-no-repeat bg-cover bg-[url('https://images.pexels.com/photos/9614109/pexels-photo-9614109.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')] antialiased">
     <div class="fixed -z-[40] inset-0 bg-black/50"></div>
     @include('landingpage.navigation')
-    <div class="pt-6 flex flex-col sm:justify-center items-center">
+    <div class="pt-6 flex flex-col  sm:justify-center items-center">
         <div
-            class="w-full sm:max-w-md mt-6 px-6 py-4 z-[50]  shadow-md overflow-hidden sm:rounded-lg rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 border border-[#eee]">
+            class="w-full sm:max-w-md  mt-6 px-6 py-4 z-[50]  shadow-md overflow-hidden sm:rounded-lg rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 border border-[#eee]">
             {{ $slot }}
         </div>
     </div>
