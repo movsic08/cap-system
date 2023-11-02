b33f7b44831049aa20a58a9979ce674adf3750db
Jericho <jerichobantiquete@gmail.com>
2023-11-02 11:23:46 +0800

    feat: landing page mobile

--- resources/views/landingpage/index.blade.php
+++ resources/views/landingpage/index.blade.php
@@ -1,18 +1,18 @@
 <x-landing-page-layout>
-    <div class="flex mt-10 flex-col justify-center  gap-4 items-center">
+    <div class="flex mt-10 flex-col justify-center  gap-4 items-center overflow-x-hidden">
         <div>
-            <h2 class="font-playfair text-6xl font-bold">
+            <h2 class="font-playfair text-4xl text-center sm:text-6xl font-bold">
                 St. Joseph Cathedral Parish
             </h2>
-            <p class="text-center mt-2 text-xl">Alaminos City, Pangasinan, Philippines</p>
+            <p class="text-center mt-2 text-lg sm:text-xl">Alaminos City, Pangasinan, Philippines</p>
 
             <div class="flex items-center gap-4 justify-center">
                 <a href="{{ route('schedule-event.index') }}"
-                    class="mt-6 flex items-center justify-center rounded-md border border-transparent bg-[#b17b16] px-8 py-3 text-base font-medium text-white hover:bg-[#926614]  focus:outline-none focus:ring-2 focus:ring-[#b17b16] focus:ring-offset-2">
+                    class="mt-6 flex items-center justify-center rounded-md border border-transparent bg-[#b17b16] px-5 sm:px-8 py-3 text-sm sm:text-base font-medium text-white hover:bg-[#926614]  focus:outline-none focus:ring-2 focus:ring-[#b17b16] focus:ring-offset-2">
                     Schedule Now
                 </a>
                 <a href="{{ route('request-certificate.index') }}"
-                    class="mt-6 flex items-center justify-center rounded-md border border-gray-300  bg-transparent px-8 py-3 text-base font-medium text-white hover:bg-gray-700  focus:outline-none focus:ring-2 focus:ring-[#b17b16] focus:ring-offset-2">
+                    class="mt-6 flex items-center justify-center rounded-md border border-gray-300  bg-transparent px-5 sm:px-8 py-3 text-sm sm:text-base font-medium text-white hover:bg-gray-700  focus:outline-none focus:ring-2 focus:ring-[#b17b16] focus:ring-offset-2">
                     Request Certificate
                 </a>
             </div>
@@ -21,17 +21,17 @@
 
         <div class="flex items-center mt-10 gap-10">
             <div
-                class="bg-slate-900 overflow-hidden outline outline-offset-8 border border-[#ffd585] outline-2 outline-[#ffd585] h-[20rem] w-[10.5rem] rounded-full">
+                class="bg-slate-900 overflow-hidden outline outline-offset-8 border border-[#ffd585] outline-2 outline-[#ffd585] h-[16rem] sm:h-[20rem] w-[9rem] sm:w-[10.5rem] rounded-full">
                 <img class="h-full object-cover w-full"
                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Guido_Reni_-_Saint_Joseph_and_the_Christ_Child_-_Google_Art_Project.jpg/240px-Guido_Reni_-_Saint_Joseph_and_the_Christ_Child_-_Google_Art_Project.jpg"
                     alt="">
             </div>
             <div
-                class="bg-slate-900 overflow-hidden outline outline-offset-8 border border-[#ffd585] outline-2 outline-[#ffd585] h-[25rem] w-[15.5rem] rounded-full">
+                class="bg-slate-900 overflow-hidden outline outline-offset-8 border border-[#ffd585] outline-2 outline-[#ffd585] h-[16rem] sm:h-[25rem] w-[9rem] sm:w-[15.5rem] rounded-full">
                 <img class="h-full object-cover w-full" src="{{ asset('background-image.jpg') }}" alt="">
             </div>
             <div
-                class="bg-slate-900 overflow-hidden outline outline-offset-8 border border-[#ffd585] outline-2 outline-[#ffd585] h-[20rem] w-[10.5rem] rounded-full">
+                class="bg-slate-900 overflow-hidden outline outline-offset-8 border border-[#ffd585] outline-2 outline-[#ffd585] h-[16rem] sm:h-[20rem] w-[9rem] sm:w-[10.5rem] rounded-full">
                 <img class="h-full object-cover w-full"
                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjb3zMMOa7OtaC2JyYLHnOhzLWhZrlCpFJqP0OxPlqSgdgwJi7GhQeQ_N5rGCQHGBXf-U&usqp=CAU"
                     alt="">
