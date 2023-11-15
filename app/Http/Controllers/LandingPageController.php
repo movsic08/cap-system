f6b1d4509556c72dd146bd754599674568a3aa33
Jericho <jerichobantiquete@gmail.com>
2023-11-15 23:11:16 +0800

    feat: burial calendar

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -116,8 +116,8 @@ class LandingPageController extends Controller
         foreach ($appointments as $appointment) {
             $events[] = [
                 'title' => 'Burial' . ' ('.$appointment->deceased_name.')',
-                'start' => $appointment->desired_start_date_time,
-                'end' => $appointment->desired_end_date_time,
+                'date' => $appointment->desired_date,
+                'time' => $appointment->desired_time,
             ];
         }
 
