ef20a3a658d61316aa60fd1f1d587bd47e86b4b4
Jericho <jerichobantiquete@gmail.com>
2023-11-22 08:25:51 +0800

    feat: print confirmation information

--- app/Http/Controllers/ExportRequestedSchedulesController.php
+++ app/Http/Controllers/ExportRequestedSchedulesController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\BaptismalSchedule;
 use App\Models\BurialSchedule;
+use App\Models\ConfirmationSchedule;
+use App\Models\WeddingSchedules;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use PhpOffice\PhpWord\TemplateProcessor;
@@ -46,7 +48,34 @@ class ExportRequestedSchedulesController extends Controller
         $templateProcessor->saveAs( "baptismal-remembrance-export" . '.docx');
         return response()->download( "baptismal-remembrance-export" . '.docx')->deleteFileAfterSend(true);
     }
-//
-// marriageschedule
-// confirmationschedule
+
+    public function marriageschedule($id) {
+        $schedule = WeddingSchedules::findOrFail($id);
+        $templateProcessor = new TemplateProcessor('Marriage-Appointment-Checklist.docx');
+        $templateProcessor->setValue('grooms_name', $schedule->grooms_name);
+        $templateProcessor->setValue('brides_name', $schedule->brides_name);
+        $templateProcessor->setValue('desired_date', Carbon::parse($schedule->desired_date)->format('M d, Y'));
+        $templateProcessor->setValue('desired_time', $schedule->desired_time);
+
+
+        $templateProcessor->saveAs( "marriage-appointment-export" . '.docx');
+        return response()->download( "marriage-appointment-export" . '.docx')->deleteFileAfterSend(true);
+    }
+
+    public function confirmationschedule($id) {
+        $schedule = ConfirmationSchedule::findOrFail($id);
+        $templateProcessor = new TemplateProcessor('Confirmation-Slip.docx');
+        $templateProcessor->setValue('confirmation_name', $schedule->confirmation_name);
+        $templateProcessor->setValue('date_of_baptism', Carbon::parse($schedule->date_of_baptism)->format('M d, Y'));
+        $templateProcessor->setValue('father_name', $schedule->father_name);
+        $templateProcessor->setValue('mother_name', $schedule->mother_name);
+        $templateProcessor->setValue('residence_of_parents', $schedule->residence_of_parents);
+        $templateProcessor->setValue('place_of_baptism', $schedule->place_of_baptism);
+        $templateProcessor->setValue('birthplace', $schedule->birthplace);
+
+
+        $templateProcessor->saveAs( "confirmation-slip-export" . '.docx');
+        return response()->download( "confirmation-slip-export" . '.docx')->deleteFileAfterSend(true);
+
+    }
 }
